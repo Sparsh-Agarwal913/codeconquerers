df5486d0182fbeb0445fb333e5aad64b51ac060d
Sparsh Agarwal <sparshaga913@gmail.com>
2024-08-01 23:49:45 +0530

    code transfered to this repo for deployment

---
+++ src/components/navbar.jsx
@@ -0,0 +1,31 @@
+"use client";
+import React from "react";
+import { FloatingNav } from "./ui/floatingNavbar";
+import { IconHome, IconMessage, IconUser } from "@tabler/icons-react";
+export function Navbar() {
+  const navItems = [
+    {
+      name: "Find Work",
+      link: "/work",
+      icon: (
+        <IconMessage className="h-4 w-4 text-neutral-500 dark:text-white" />
+      ),
+    },
+    {
+      name: "About",
+      link: "/about",
+      icon: <IconUser className="h-4 w-4 text-neutral-500 dark:text-white" />,
+    },
+    {
+      name: "Resources",
+      link: "/resources",
+      icon: <IconUser className="h-4 w-4 text-neutral-500 dark:text-white" />,
+    },
+  ];
+  return (
+    <div className="relative  w-full flex-row">
+      <FloatingNav navItems={navItems} />
+    </div>
+  );
+}
+
